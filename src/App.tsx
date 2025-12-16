cf0b194251687ddc681eb85e09580b719174c579
Christian Socias <Socias_93@hotmail.com>
2025-12-16 22:11:02 +0100

    fixed tablelist

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,26 @@ function App() {
   const items = getItems();
   return (
     <>
-      <table
-        className="table-fixed
-">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item._id}>
-              <td> {item.name} </td>
-              <td> {item.category.name} </td>
-              <td> {item.numberInStock} </td>
+      <div className="bg-dark text-primary p-3 ">
+        <table className="table table-dark table-bordered border-primary ">
+          <thead>
+            <tr>
+              <th className="text-primary">Name</th>
+              <th className="text-primary">Category</th>
+              <th className="text-primary">Stock</th>
             </tr>
-          ))}
-          ;
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item._id}>
+                <td> {item.name} </td>
+                <td> {item.category.name} </td>
+                <td> {item.numberInStock} st </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
