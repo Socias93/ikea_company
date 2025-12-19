7d51cdf21b482053abb756834730c470121fd819
Christian Socias <Socias_93@hotmail.com>
2025-12-19 20:13:55 +0100

    added price to the items

--- src/components/ItemsTable.tsx
+++ src/components/ItemsTable.tsx
@@ -38,6 +38,8 @@ function ItemsTable() {
             <tr>
               <th className="text-primary">Name</th>
               <th className="text-primary">Category</th>
+              <th className="text-primary">Price - (Kr)</th>
+
               <th className="text-primary">Stock</th>
             </tr>
           </thead>
@@ -46,6 +48,8 @@ function ItemsTable() {
               <tr key={item._id}>
                 <td> {item.name} </td>
                 <td> {item.category.name} </td>
+                <td> {item.price} kr </td>
+
                 <td> {item.numberInStock} st </td>
               </tr>
             ))}
