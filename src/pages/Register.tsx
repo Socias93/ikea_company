5f0cb4d7bb2735f41c65fc32a1a5bf724018f6c3
Christian Socias <Socias_93@hotmail.com>
2025-12-20 15:22:52 +0100

    fixed complete form for registerpage

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { z } from "zod";
 
 const schema = z.object({
@@ -16,28 +17,55 @@ const schema = z.object({
 type registerData = z.infer<typeof schema>;
 
 function Register() {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<registerData>({ resolver: zodResolver(schema) });
+
+  function onSubmit(data: registerData) {
+    console.log("Submitted", data);
+    navigate("/");
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="vh-100 d-grid justify-content-center align-content-center">
           <h3 className="text-primary mb-3 text-center">Register page</h3>
           <div className="p-3 shadow rounded-3">
             <div className="mb-3 text-primary">
               <label className="form-label">Name</label>
-              <input type="text" className="form-control" />
+              <input
+                {...register("name")}
+                type="text"
+                className="form-control"
+              />
+              {errors.name && (
+                <p className="text-danger"> {errors.name.message} </p>
+              )}
             </div>
             <div className="mb-3 text-primary">
               <label className="form-label">Email</label>
-              <input type="text" className="form-control" />
+              <input
+                {...register("email")}
+                type="text"
+                className="form-control"
+              />
+              {errors.email && (
+                <p className="text-danger"> {errors.email.message} </p>
+              )}
             </div>
             <div className="mb-3 text-primary">
               <label className="form-label">Password</label>
-              <input type="text" className="form-control" />
+              <input
+                {...register("password")}
+                type="text"
+                className="form-control"
+              />
+              {errors.password && (
+                <p className="text-danger"> {errors.password.message} </p>
+              )}
             </div>
             <div className="text-center m-2">
               <button className="btn btn-outline-primary">Save</button>
