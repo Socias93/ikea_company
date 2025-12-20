0505bd71f872437f53419a0a046d3c3dc865f833
Christian Socias <Socias_93@hotmail.com>
2025-12-20 15:10:04 +0100

    added form for registerpage

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,30 @@
 function Register() {
-  return <h1 className="text-primary p-3">Register</h1>;
+  return (
+    <>
+      <form>
+        <div className="vh-100 d-grid justify-content-center align-content-center">
+          <h3 className="text-primary mb-3 text-center">Register page</h3>
+          <div className="p-3 shadow rounded-3">
+            <div className="mb-3 text-primary">
+              <label className="form-label">Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="mb-3 text-primary">
+              <label className="form-label">Email</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="mb-3 text-primary">
+              <label className="form-label">Password</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="text-center m-2">
+              <button className="btn btn-outline-primary">Save</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Register;
