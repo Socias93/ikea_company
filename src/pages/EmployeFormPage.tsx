339916f83807ed5b3716e6dfdaa062eba6e68386
Christian Socias <Socias_93@hotmail.com>
2025-12-23 16:33:50 +0100

    added errormessage to employeform

--- src/pages/EmployeFormPage.tsx
+++ src/pages/EmployeFormPage.tsx
@@ -15,7 +15,6 @@ function EmployeFormPage() {
   return (
     <>
       <div className="vh-100 d-grid justify-content-center align-content-center">
-        <h1 className="text-primary text-center">EmployeFormPage</h1>;
         <div
           className="p-3 shadow rounded-3"
           style={{ width: "500px", maxWidth: "500px" }}>
@@ -23,30 +22,59 @@ function EmployeFormPage() {
             <div className="mb-3">
               <label className="form-label">Name</label>
               <input {...register("name")} className="form-control" />
+              <div style={{ minHeight: "20px" }}>
+                {errors.name && (
+                  <p className="text-danger">{errors.name.message} </p>
+                )}
+              </div>
             </div>
             <div className="mb-3">
-              <label {...register("age")} className="form-label">
+              <label
+                {...register("age", { valueAsNumber: true })}
+                className="form-label">
                 Age
               </label>
               <input className="form-control" />
+              <div style={{ minHeight: "20px" }}>
+                {errors.age && (
+                  <p className="text-danger">{errors.age.message} </p>
+                )}
+              </div>
             </div>
             <div className="mb-3">
               <label {...register("email")} className="form-label">
                 Email
               </label>
               <input className="form-control" />
+              <div style={{ minHeight: "20px" }}>
+                {errors.email && (
+                  <p className="text-danger">{errors.email.message} </p>
+                )}
+              </div>
             </div>
             <div className="mb-3">
-              <label {...register("phone")} className="form-label">
+              <label
+                {...register("phone", { valueAsNumber: true })}
+                className="form-label">
                 Phone
               </label>
               <input className="form-control" />
+              <div style={{ minHeight: "20px" }}>
+                {errors.phone && (
+                  <p className="text-danger">{errors.phone.message} </p>
+                )}
+              </div>
             </div>
             <div className="mb-3">
               <label {...register("role")} className="form-label">
                 Role
               </label>
               <input className="form-control" />
+              <div style={{ minHeight: "20px" }}>
+                {errors.role && (
+                  <p className="text-danger">{errors.role.message} </p>
+                )}
+              </div>
             </div>
             <div className="text-center">
               <button className="btn btn-outline-primary w-50">Save</button>
--- src/pages/schemas/EmployeSchema.ts
+++ src/pages/schemas/EmployeSchema.ts
@@ -3,11 +3,15 @@ import z from "zod";
 export const schema = z.object({
   id: z.string().optional(),
   name: z.string().min(1, { message: "Name is required" }),
-  age: z.number().min(18, { message: "Age cant be less than 18" }),
+  age: z
+    .number({ error: "You must write with numbers" })
+    .min(18, { message: "Age cant be less than 18" }),
   email: z
     .email({ error: "Wrong email format" })
     .min(1, { message: "Email is required" }),
-  phone: z.number().min(1, { message: "Phonenumber is required" }),
+  phone: z
+    .number({ error: "You must write with numbers" })
+    .min(1, { message: "Phonenumber is required" }),
   role: z.string().min(1, { message: "Role is required" }),
 });
 
