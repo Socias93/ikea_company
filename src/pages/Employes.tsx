58ebf6073a68389ecebfce09a00301f1cff1b74b
Christian Socias <Socias_93@hotmail.com>
2025-12-23 15:45:47 +0100

    fixed employepage

--- src/pages/Employes.tsx
+++ src/pages/Employes.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { getEmployes } from "../services/FakeEmployeService";
+
 function Employes() {
-  return <h1 className="text-primary p-3">Employes</h1>;
+  const [employes, setEmployes] = useState(getEmployes());
+  return (
+    <div className="container py-4">
+      <div className="row g-3 justify-content-center">
+        {employes.map((t) => (
+          <div key={t.id} className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex">
+            <div className="card shadow-lg border-0 rounded-4 w-100 d-flex flex-column">
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title text-center mb-3">{t.name}</h5>
+                <h6> {t.email} </h6>
+                <span>+46-{t.number} </span>
+                <p className="text-muted fw-bold">{t.role} </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Employes;
