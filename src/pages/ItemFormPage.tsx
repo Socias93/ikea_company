e9988c0074e765c71882719895ed66bbd4179ec8
Christian Socias <Socias_93@hotmail.com>
2025-12-27 19:50:19 +0100

    fixed reset for prefilledup schema if you change to create new item

--- src/pages/ItemFormPage.tsx
+++ src/pages/ItemFormPage.tsx
@@ -24,7 +24,15 @@ function ItemFormPage() {
       if (!category) return;
       setCategories(categories);
 
-      if (!id || id === "new/item") return;
+      if (!id || id === "new/item") {
+        reset({
+          name: "",
+          categoryId: "",
+          numberInStock: null,
+          price: null,
+        } as any);
+        return;
+      }
       const item = getItem(id);
       if (!item) return;
       reset(mapToItemData(item));
