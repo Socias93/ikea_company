053462f05ce246274dc5a776b5194bb500cc1885
Christian Socias <Socias_93@hotmail.com>
2025-12-27 19:26:39 +0100

    added edit button to items

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -9,7 +9,10 @@ function TableBody({ items }: Props) {
     <tbody>
       {items.map((item) => (
         <tr key={item.id}>
-          <td> {item.name} </td>
+          <td className="d-flex justify-content-between">
+            {item.name}
+            <button className="btn btn-outline-primary">Edit</button>
+          </td>
           <td> {item.category.name} </td>
           <td> {item.price} kr </td>
           <td> {item.numberInStock} st </td>
