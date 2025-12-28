9526ef9cda463a531e8714705ecc71596c5eff96
Christian Socias <Socias_93@hotmail.com>
2025-12-28 20:55:35 +0100

    added search functionality on categorypage

--- src/pages/AllCategories.tsx
+++ src/pages/AllCategories.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { getCategories } from "../services/fakeCategoryService";
 import { getItems } from "../services/fakeItemService";
 import { useState } from "react";
@@ -7,6 +7,15 @@ function AllCategories() {
   const [categories, setCategories] = useState(getCategories());
   const items = getItems();
   const navigate = useNavigate();
+  const { searchValue } = useOutletContext<{
+    searchValue: string;
+  }>();
+
+  const query = searchValue.toLowerCase();
+
+  let filtredCategories = categories.filter((category) =>
+    category.name.toLowerCase().includes(query)
+  );
 
   function handleDelete(id: string) {
     const newCategory = categories.filter((category) => category.id !== id);
@@ -16,7 +25,7 @@ function AllCategories() {
     <>
       <div className="container py-4">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3">
-          {categories.map((category) => {
+          {filtredCategories.map((category) => {
             const usedCount = items.filter(
               (it) => it.category.id === category.id
             ).length;
